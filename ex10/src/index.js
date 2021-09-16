be3214d91246ee125872d1a22976258e842c3eac
misokralj <miki.t@t-com.me>
2021-09-17 01:07:08 +0200

    Exercise 10

---
+++ ex10/src/index.js
@@ -0,0 +1,16 @@
+function main() {
+  // only change code below this line
+  var myFirstName = "Miomir";
+  var myLastName = "Turkovic";
+  var myHomeTown = "Podgorica";
+  // only change code above this line
+  return {
+    myFirstName,
+    myLastName,
+    myHomeTown,
+  };
+}
+
+console.log(main());
+
+module.exports = main;
